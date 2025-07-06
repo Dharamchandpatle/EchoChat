6c151e98a51640a3f1b9753957b28448b95825df
Dharamchand <dharamchandpatle917@gmail.com>
2025-07-07 00:47:58 +0530

    final done + npm build merge frontend and backend

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
         </Link>
 
         <Link
-          to="/friends"
+          to="/"
           className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
             currentPath === "/friends" ? "btn-active" : ""
           }`}
